9586400a8b0ea1d2b42f96749aedb1410ddc572d
pawan <spawan316@gmail.com>
2022-07-11 15:41:43 +0545

    Added about and image loader

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+import { Box, Typography} from "@material-ui/core";
+
+export default function Home() {
+  return (
+    <Box>
+        <Typography>
+            Hi
+        </Typography>
+    </Box>
+    
+  )
+}
